c623d3f6971e448022472632927fc83a8a004540
Thales <thales.souz@outlook.com>
2023-07-20 19:02:22 -0300

    feat(server): products controller

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -6,6 +6,8 @@ import {
   Patch,
   Param,
   Delete,
+  NotFoundException,
+  ConflictException,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -16,7 +18,13 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  async create(@Body() createProductDto: CreateProductDto) {
+    const product = await this.productsService.findOne({
+      name: createProductDto.name,
+    });
+
+    if (product) throw new ConflictException(`This product already exists`);
+
     return this.productsService.create(createProductDto);
   }
 
@@ -27,16 +35,21 @@ export class ProductsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+    const product = this.productsService.findOne({ _id: id });
+
+    if (!product)
+      throw new NotFoundException(`Product with id: '${id}' not found`);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  async remove(@Param('id') id: string) {
+    await this.productsService.findOne({ _id: id });
+
+    return this.productsService.remove(id);
   }
 }
