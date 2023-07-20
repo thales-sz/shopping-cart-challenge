ba16195db04080bfa64fa0fe019b9fdae7351420
Thales <thales.souz@outlook.com>
2023-07-20 18:32:41 -0300

    feat(server): add whitelist for validation schema

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const logger = new Logger(AppModule.name);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
+
+  await app.listen(configService.get('PORT'));
+
+  logger.log(`Server is running on ${await app.getUrl()}`);
 }
+
 bootstrap();
